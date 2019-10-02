7220443cdcb150ab9691bd460223c2a5e09a394f
Kimiyuki Yamauchi <yamauchi@std.it-college.ac.jp>
2019-10-02 12:41:52 +0900

    Playerクラスの定義

---
+++ src/jan/ObjectJanken.java
@@ -0,0 +1,9 @@
+/**
+ * オブジェクト指向によるジャンケンプログラム。
+ */
+public class ObjectJanken {
+    public static void main(String[] args) {
+        Player player = new Player("村田さん");
+        System.out.println(player.getName());
+    }
+}
