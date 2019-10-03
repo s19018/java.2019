fddb0edf685afdeb6ef2dc264eeba1f7428f4bcb
Kimiyuki Yamauchi <yamauchi@std.it-college.ac.jp>
2019-10-03 11:47:59 +0900

    オブジェクト指向を用いたジャンケンプログラム(完成版)

--- src/jan/ObjectJanken.java
+++ src/jan/ObjectJanken.java
@@ -3,7 +3,16 @@
  */
 public class ObjectJanken {
     public static void main(String[] args) {
-        Player player = new Player("村田さん");
-        System.out.println(player.getName());
+        // 審判(斎藤さん)のインスタンス生成
+        Judge saito = new Judge();
+
+        // プレイヤー１(村田さん)の生成
+        Player murata = new Player("村田さん");
+
+        // プレイヤー２(山田さん)の生成
+        Player yamada = new Player("山田さん");
+
+        // 村田さんと山田さんをプレイヤーとしてジャンケンを開始する
+        saito.startJanken(murata, yamada);
     }
 }
