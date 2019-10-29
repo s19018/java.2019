e920abfb28ee246e7f33a501bfeb748b8e700eea
Kimiyuki Yamauchi <yamauchi@std.it-college.ac.jp>
2019-10-29 12:33:26 +0900

    トランプゲームフレームワークを使った七並べ(完成版)

--- src/fantan/Fantan.java
+++ src/fantan/Fantan.java
@@ -14,35 +14,56 @@ import trump.Rule;
 public class Fantan
 {
     public static void main(String [] args) {
-        Table table = new FantanTable();
-        System.out.println(table);
-        Card [] cards = {new Card(Card.SUIT_HEART, 1)};
-        table.putCard(cards);
-        cards[0] = new Card(Card.SUIT_CLUB, 13);
-        table.putCard(cards);
-        cards[0] = new Card(Card.SUIT_DIAMOND, 5);
-        table.putCard(cards);
-        cards[0] = new Card(Card.SUIT_SPADE, 8);
-        table.putCard(cards);
-        System.out.println(table);
 
-        Hand hand = new Hand();
-        hand.addCard(new Card(Card.SUIT_SPADE, 7));
-        hand.addCard(new Card(Card.SUIT_DIAMOND, 7));
-        Rule rule = new FantanRule();
-        cards = rule.findCandidate(hand, table);
-        System.out.println(cards[0]);
-        cards = rule.findCandidate(hand, table);
-        if (cards == null) {
-            System.out.println("not found");
-        } else {
-            System.out.println("error");
-        }
-        
+      // 進行役の生成
+      Master master = new FantanMaster();
 
-        // Card[][] tmp = table.getCards();
-        // print_c(tmp);
+      // テーブルの生成
+      Table table = new FantanTable();
+
+      // ルールの生成
+      Rule rule = new FantanRule();
+
+      // プレイヤーの生成
+      Player murata = new FantanPlayer("村田", master, table, rule);
+      Player yamada = new FantanPlayer("山田", master, table, rule);
+      Player saito = new FantanPlayer("斎藤", master, table, rule);
+
+      // プレイヤーの登録
+      master.registerPlayer(murata);
+      master.registerPlayer(yamada);
+      master.registerPlayer(saito);
+
+      // トランプを生成する
+      Hand trump = createTrump();
+
+      // ゲームの準備をする
+      master.prepareGame(trump);
+
+      // ゲームを開始する
+      master.startGame();
     }
+
+    /**
+     * 52枚のトランプを生成する。
+     *
+     * @return トランプを格納したHand
+     */
+    private static Hand createTrump() {
+        Hand trump = new Hand();
+
+        // 各スート52枚のカードを生成する
+
+        for (int number = 1; number <= 13; number++) {
+            trump.addCard(new Card(Card.SUIT_CLUB, number));
+            trump.addCard(new Card(Card.SUIT_DIAMOND, number));
+            trump.addCard(new Card(Card.SUIT_HEART, number));
+            trump.addCard(new Card(Card.SUIT_SPADE, number));
+
+        }
+        return trump;
+    }
+
 
     private static void print_c(Card[][] c) {
         StringBuffer tableString = new StringBuffer(200);
