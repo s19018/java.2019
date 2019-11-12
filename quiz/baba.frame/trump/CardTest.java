8ed97f1e276430bcebd8896a06c0b1d293fe99ab
Kimiyuki Yamauchi <yamauchi@std.it-college.ac.jp>
2019-11-12 23:28:16 +0900

    構成の変更

--- quiz/baba.frame/trump/CardTest.java
+++ quiz/baba.frame/trump/CardTest.java
@@ -2,388 +2,205 @@ import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
 import org.junit.Test;
 import org.junit.Before;
-// import org.junit.experimental.Enclosed;
+import org.junit.runner.RunWith;
+import org.junit.experimental.theories.Theory;
+import org.junit.experimental.theories.DataPoints;
+import org.junit.experimental.runners.Enclosed;
+import org.junit.experimental.theories.Theories;
 import trump.Card;
 
-// @RunWith(Enclosed.class)
+@RunWith(Enclosed.class)
 public class CardTest {
 
-  private Card card;
+  @RunWith(Theories.class)
+  public static class toStringの動作確認 {
+    private Card card;
 
-  @Before
-  public void setUp() throws Exception {
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @DataPoints
+    public static Fixture[] PARAMs = {
+      new Fixture(Card.SUIT_CLUB, 1, "CA"),
+      new Fixture(Card.SUIT_CLUB, 2, "C2"),
+      new Fixture(Card.SUIT_CLUB, 3, "C3"),
+      new Fixture(Card.SUIT_CLUB, 4, "C4"),
+      new Fixture(Card.SUIT_CLUB, 5, "C5"),
+      new Fixture(Card.SUIT_CLUB, 6, "C6"),
+      new Fixture(Card.SUIT_CLUB, 7, "C7"),
+      new Fixture(Card.SUIT_CLUB, 8, "C8"),
+      new Fixture(Card.SUIT_CLUB, 9, "C9"),
+      new Fixture(Card.SUIT_CLUB, 10, "CT"),
+      new Fixture(Card.SUIT_CLUB, 11, "CJ"),
+      new Fixture(Card.SUIT_CLUB, 12, "CQ"),
+      new Fixture(Card.SUIT_CLUB, 13, "CK"),
+      new Fixture(Card.SUIT_DIAMOND, 1, "DA"),
+      new Fixture(Card.SUIT_DIAMOND, 2, "D2"),
+      new Fixture(Card.SUIT_DIAMOND, 3, "D3"),
+      new Fixture(Card.SUIT_DIAMOND, 4, "D4"),
+      new Fixture(Card.SUIT_DIAMOND, 5, "D5"),
+      new Fixture(Card.SUIT_DIAMOND, 6, "D6"),
+      new Fixture(Card.SUIT_DIAMOND, 7, "D7"),
+      new Fixture(Card.SUIT_DIAMOND, 8, "D8"),
+      new Fixture(Card.SUIT_DIAMOND, 9, "D9"),
+      new Fixture(Card.SUIT_DIAMOND, 10, "DT"),
+      new Fixture(Card.SUIT_DIAMOND, 11, "DJ"),
+      new Fixture(Card.SUIT_DIAMOND, 12, "DQ"),
+      new Fixture(Card.SUIT_DIAMOND, 13, "DK"),
+      new Fixture(Card.SUIT_HEART, 1, "HA"),
+      new Fixture(Card.SUIT_HEART, 2, "H2"),
+      new Fixture(Card.SUIT_HEART, 3, "H3"),
+      new Fixture(Card.SUIT_HEART, 4, "H4"),
+      new Fixture(Card.SUIT_HEART, 5, "H5"),
+      new Fixture(Card.SUIT_HEART, 6, "H6"),
+      new Fixture(Card.SUIT_HEART, 7, "H7"),
+      new Fixture(Card.SUIT_HEART, 8, "H8"),
+      new Fixture(Card.SUIT_HEART, 9, "H9"),
+      new Fixture(Card.SUIT_HEART, 10, "HT"),
+      new Fixture(Card.SUIT_HEART, 11, "HJ"),
+      new Fixture(Card.SUIT_HEART, 12, "HQ"),
+      new Fixture(Card.SUIT_HEART, 13, "HK"),
+      new Fixture(Card.SUIT_SPADE, 1, "SA"),
+      new Fixture(Card.SUIT_SPADE, 2, "S2"),
+      new Fixture(Card.SUIT_SPADE, 3, "S3"),
+      new Fixture(Card.SUIT_SPADE, 4, "S4"),
+      new Fixture(Card.SUIT_SPADE, 5, "S5"),
+      new Fixture(Card.SUIT_SPADE, 6, "S6"),
+      new Fixture(Card.SUIT_SPADE, 7, "S7"),
+      new Fixture(Card.SUIT_SPADE, 8, "S8"),
+      new Fixture(Card.SUIT_SPADE, 9, "S9"),
+      new Fixture(Card.SUIT_SPADE, 10, "ST"),
+      new Fixture(Card.SUIT_SPADE, 11, "SJ"),
+      new Fixture(Card.SUIT_SPADE, 12, "SQ"),
+      new Fixture(Card.SUIT_SPADE, 13, "SK"),
+    };
+
+
+    @Theory
+    public void toStringの動作確認(Fixture p) throws Exception{
+      card = new Card(p.suit, p.number);
+      String ret = card.toString();
+      assertThat(ret, is(p.expected));
+    }
+
+    static class Fixture {
+      int suit;
+      int number;
+      String expected;
+
+      Fixture(int suit, int number, String expected) {
+        this.suit = suit;
+        this.number = number;
+        this.expected = expected;
+      }
+    }
   }
 
-  @Test
-  public void toString_CLUB_1() {
-    card = new Card(Card.SUIT_CLUB, 1);
-    String ret = card.toString();
-    assertThat(ret, is("CA"));
+
+  public static class コンストラクタの確認 {
+    @Test
+    public void コンストラクタ() throws Exception{
+      Card instance = new Card(Card.SUIT_CLUB, 1);
+      assertThat(instance.getSuit(), is(Card.SUIT_CLUB));
+      assertThat(instance.getNumber(), is(1));
+    }
   }
 
-  @Test
-  public void toString_CLUB_2() {
-    card = new Card(Card.SUIT_CLUB, 2);
-    String ret = card.toString();
-    assertThat(ret, is("C2"));
+  @RunWith(Theories.class)
+  public static class equalsの動作確認 {
+    private Card card;
+    private Card other;
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @DataPoints
+    public static Fixture[] PARAMs = {
+      new Fixture(Card.SUIT_CLUB, 1, Card.SUIT_CLUB, 10, false),
+      new Fixture(Card.SUIT_CLUB, 2, Card.SUIT_CLUB, 10, false),
+      new Fixture(Card.SUIT_CLUB, 3, Card.SUIT_CLUB, 10, false),
+      new Fixture(Card.SUIT_CLUB, 4, Card.SUIT_CLUB, 10, false),
+      new Fixture(Card.SUIT_CLUB, 5, Card.SUIT_CLUB, 10, false),
+      new Fixture(Card.SUIT_CLUB, 6, Card.SUIT_CLUB, 10, false),
+      new Fixture(Card.SUIT_CLUB, 7, Card.SUIT_CLUB, 10, false),
+      new Fixture(Card.SUIT_CLUB, 8, Card.SUIT_CLUB, 10, false),
+      new Fixture(Card.SUIT_CLUB, 9, Card.SUIT_CLUB, 10, false),
+      new Fixture(Card.SUIT_CLUB, 10, Card.SUIT_CLUB, 10, true),
+      new Fixture(Card.SUIT_CLUB, 11, Card.SUIT_CLUB, 10, false),
+      new Fixture(Card.SUIT_CLUB, 12, Card.SUIT_CLUB, 10, false),
+      new Fixture(Card.SUIT_CLUB, 13, Card.SUIT_CLUB, 10, false),
+      new Fixture(Card.SUIT_DIAMOND, 1, Card.SUIT_HEART, 13, false),
+      new Fixture(Card.SUIT_DIAMOND, 2, Card.SUIT_HEART, 13, false),
+      new Fixture(Card.SUIT_DIAMOND, 3, Card.SUIT_HEART, 13, false),
+      new Fixture(Card.SUIT_DIAMOND, 4, Card.SUIT_HEART, 13, false),
+      new Fixture(Card.SUIT_DIAMOND, 5, Card.SUIT_HEART, 13, false),
+      new Fixture(Card.SUIT_DIAMOND, 6, Card.SUIT_HEART, 13, false),
+      new Fixture(Card.SUIT_DIAMOND, 7, Card.SUIT_HEART, 13, false),
+      new Fixture(Card.SUIT_DIAMOND, 8, Card.SUIT_HEART, 13, false),
+      new Fixture(Card.SUIT_DIAMOND, 9, Card.SUIT_HEART, 13, false),
+      new Fixture(Card.SUIT_DIAMOND, 10, Card.SUIT_HEART, 13, false),
+      new Fixture(Card.SUIT_DIAMOND, 11, Card.SUIT_HEART, 13, false),
+      new Fixture(Card.SUIT_DIAMOND, 12, Card.SUIT_HEART, 13, false),
+      new Fixture(Card.SUIT_DIAMOND, 13, Card.SUIT_DIAMOND, 13, true),
+      new Fixture(Card.SUIT_HEART, 1, Card.SUIT_SPADE, 2, false),
+      new Fixture(Card.SUIT_HEART, 2, Card.SUIT_HEART, 2, true),
+      new Fixture(Card.SUIT_HEART, 3, Card.SUIT_SPADE, 2, false),
+      new Fixture(Card.SUIT_HEART, 4, Card.SUIT_SPADE, 2, false),
+      new Fixture(Card.SUIT_HEART, 5, Card.SUIT_SPADE, 2, false),
+      new Fixture(Card.SUIT_HEART, 6, Card.SUIT_SPADE, 2, false),
+      new Fixture(Card.SUIT_HEART, 7, Card.SUIT_SPADE, 2, false),
+      new Fixture(Card.SUIT_HEART, 8, Card.SUIT_SPADE, 2, false),
+      new Fixture(Card.SUIT_HEART, 9, Card.SUIT_SPADE, 2, false),
+      new Fixture(Card.SUIT_HEART, 10, Card.SUIT_SPADE, 2, false),
+      new Fixture(Card.SUIT_HEART, 11, Card.SUIT_SPADE, 2, false),
+      new Fixture(Card.SUIT_HEART, 12, Card.SUIT_SPADE, 2, false),
+      new Fixture(Card.SUIT_HEART, 13, Card.SUIT_SPADE, 2, false),
+      new Fixture(Card.SUIT_SPADE, 1, Card.SUIT_CLUB, 1, false),
+      new Fixture(Card.SUIT_SPADE, 2, Card.SUIT_CLUB, 1, false),
+      new Fixture(Card.SUIT_SPADE, 3, Card.SUIT_CLUB, 1, false),
+      new Fixture(Card.SUIT_SPADE, 4, Card.SUIT_CLUB, 1, false),
+      new Fixture(Card.SUIT_SPADE, 5, Card.SUIT_CLUB, 1, false),
+      new Fixture(Card.SUIT_SPADE, 6, Card.SUIT_CLUB, 1, false),
+      new Fixture(Card.SUIT_SPADE, 7, Card.SUIT_CLUB, 1, false),
+      new Fixture(Card.SUIT_SPADE, 8, Card.SUIT_CLUB, 1, false),
+      new Fixture(Card.SUIT_SPADE, 9, Card.SUIT_CLUB, 1, false),
+      new Fixture(Card.SUIT_SPADE, 10, Card.SUIT_CLUB, 1, false),
+      new Fixture(Card.SUIT_SPADE, 11, Card.SUIT_CLUB, 1, false),
+      new Fixture(Card.SUIT_SPADE, 12, Card.SUIT_CLUB, 1, false),
+      new Fixture(Card.SUIT_SPADE, 13, Card.SUIT_CLUB, 1, false),
+    };
+
+
+    @Theory
+    public void equalsの動作確認(Fixture p) throws Exception{
+      // debug
+      // System.out.println(" mysuit = " + p.mysuit + " mynumber = " + p.mynumber);
+      // System.out.println(" othersuit = " + p.othersuit + " othernumber = " + p.othernumber);
+      card = new Card(p.mysuit, p.mynumber);
+      other = new Card(p.othersuit, p.othernumber);
+      boolean ret = card.equals(other);
+      assertThat(ret, is(p.expected));
+    }
+
+    static class Fixture {
+      int mysuit;
+      int mynumber;
+      int othersuit;
+      int othernumber;
+      boolean expected;
+
+      Fixture(int mysuit, int mynumber, int othersuit, int othernumber, boolean expected) {
+        this.mysuit = mysuit;
+        this.mynumber = mynumber;
+        this.othersuit = othersuit;
+        this.othernumber = othernumber;
+        this.expected = expected;
+      }
+    }
   }
 
-  @Test
-  public void toString_CLUB_3() {
-    card = new Card(Card.SUIT_CLUB, 3);
-    String ret = card.toString();
-    assertThat(ret, is("C3"));
-  }
-
-  @Test
-  public void toString_CLUB_4() {
-    card = new Card(Card.SUIT_CLUB, 4);
-    String ret = card.toString();
-    assertThat(ret, is("C4"));
-  }
-
-  @Test
-  public void toString_CLUB_5() {
-    card = new Card(Card.SUIT_CLUB, 5);
-    String ret = card.toString();
-    assertThat(ret, is("C5"));
-  }
-
-  @Test
-  public void toString_CLUB_6() {
-    card = new Card(Card.SUIT_CLUB, 6);
-    String ret = card.toString();
-    assertThat(ret, is("C6"));
-  }
-
-  @Test
-  public void toString_CLUB_7() {
-    card = new Card(Card.SUIT_CLUB, 7);
-    String ret = card.toString();
-    assertThat(ret, is("C7"));
-  }
-
-  @Test
-  public void toString_CLUB_8() {
-    card = new Card(Card.SUIT_CLUB, 8);
-    String ret = card.toString();
-    assertThat(ret, is("C8"));
-  }
-
-  @Test
-  public void toString_CLUB_9() {
-    card = new Card(Card.SUIT_CLUB, 9);
-    String ret = card.toString();
-    assertThat(ret, is("C9"));
-  }
-
-  @Test
-  public void toString_CLUB_10() {
-    card = new Card(Card.SUIT_CLUB, 10);
-    String ret = card.toString();
-    assertThat(ret, is("CT"));
-  }
-
-  @Test
-  public void toString_CLUB_11() {
-    card = new Card(Card.SUIT_CLUB, 11);
-    String ret = card.toString();
-    assertThat(ret, is("CJ"));
-  }
-
-  @Test
-  public void toString_CLUB_12() {
-    card = new Card(Card.SUIT_CLUB, 12);
-    String ret = card.toString();
-    assertThat(ret, is("CQ"));
-  }
-
-  @Test
-  public void toString_CLUB_13() {
-    card = new Card(Card.SUIT_CLUB, 13);
-    String ret = card.toString();
-    assertThat(ret, is("CK"));
-  }
-
-  @Test
-  public void toString_HEART_1() {
-    card = new Card(Card.SUIT_HEART, 1);
-    String ret = card.toString();
-    assertThat(ret, is("HA"));
-  }
-
-  @Test
-  public void toString_HEART_2() {
-    card = new Card(Card.SUIT_HEART, 2);
-    String ret = card.toString();
-    assertThat(ret, is("H2"));
-  }
-
-  @Test
-  public void toString_HEART_3() {
-    card = new Card(Card.SUIT_HEART, 3);
-    String ret = card.toString();
-    assertThat(ret, is("H3"));
-  }
-
-  @Test
-  public void toString_HEART_4() {
-    card = new Card(Card.SUIT_HEART, 4);
-    String ret = card.toString();
-    assertThat(ret, is("H4"));
-  }
-
-  @Test
-  public void toString_HEART_5() {
-    card = new Card(Card.SUIT_HEART, 5);
-    String ret = card.toString();
-    assertThat(ret, is("H5"));
-  }
-
-  @Test
-  public void toString_HEART_6() {
-    card = new Card(Card.SUIT_HEART, 6);
-    String ret = card.toString();
-    assertThat(ret, is("H6"));
-  }
-
-  @Test
-  public void toString_HEART_7() {
-    card = new Card(Card.SUIT_HEART, 7);
-    String ret = card.toString();
-    assertThat(ret, is("H7"));
-  }
-
-  @Test
-  public void toString_HEART_8() {
-    card = new Card(Card.SUIT_HEART, 8);
-    String ret = card.toString();
-    assertThat(ret, is("H8"));
-  }
-
-  @Test
-  public void toString_HEART_9() {
-    card = new Card(Card.SUIT_HEART, 9);
-    String ret = card.toString();
-    assertThat(ret, is("H9"));
-  }
-
-  @Test
-  public void toString_HEART_10() {
-    card = new Card(Card.SUIT_HEART, 10);
-    String ret = card.toString();
-    assertThat(ret, is("HT"));
-  }
-
-  @Test
-  public void toString_HEART_11() {
-    card = new Card(Card.SUIT_HEART, 11);
-    String ret = card.toString();
-    assertThat(ret, is("HJ"));
-  }
-
-  @Test
-  public void toString_HEART_12() {
-    card = new Card(Card.SUIT_HEART, 12);
-    String ret = card.toString();
-    assertThat(ret, is("HQ"));
-  }
-
-  @Test
-  public void toString_HEART_13() {
-    card = new Card(Card.SUIT_HEART, 13);
-    String ret = card.toString();
-    assertThat(ret, is("HK"));
-  }
-
-  @Test
-  public void toString_DIAMOND_1() {
-    card = new Card(Card.SUIT_DIAMOND, 1);
-    String ret = card.toString();
-    assertThat(ret, is("DA"));
-  }
-
-  @Test
-  public void toString_DIAMOND_2() {
-    card = new Card(Card.SUIT_DIAMOND, 2);
-    String ret = card.toString();
-    assertThat(ret, is("D2"));
-  }
-
-  @Test
-  public void toString_DIAMOND_3() {
-    card = new Card(Card.SUIT_DIAMOND, 3);
-    String ret = card.toString();
-    assertThat(ret, is("D3"));
-  }
-
-  @Test
-  public void toString_DIAMOND_4() {
-    card = new Card(Card.SUIT_DIAMOND, 4);
-    String ret = card.toString();
-    assertThat(ret, is("D4"));
-  }
-
-  @Test
-  public void toString_DIAMOND_5() {
-    card = new Card(Card.SUIT_DIAMOND, 5);
-    String ret = card.toString();
-    assertThat(ret, is("D5"));
-  }
-
-  @Test
-  public void toString_DIAMOND_6() {
-    card = new Card(Card.SUIT_DIAMOND, 6);
-    String ret = card.toString();
-    assertThat(ret, is("D6"));
-  }
-
-  @Test
-  public void toString_DIAMOND_7() {
-    card = new Card(Card.SUIT_DIAMOND, 7);
-    String ret = card.toString();
-    assertThat(ret, is("D7"));
-  }
-
-  @Test
-  public void toString_DIAMOND_8() {
-    card = new Card(Card.SUIT_DIAMOND, 8);
-    String ret = card.toString();
-    assertThat(ret, is("D8"));
-  }
-
-  @Test
-  public void toString_DIAMOND_9() {
-    card = new Card(Card.SUIT_DIAMOND, 9);
-    String ret = card.toString();
-    assertThat(ret, is("D9"));
-  }
-
-  @Test
-  public void toString_DIAMOND_10() {
-    card = new Card(Card.SUIT_DIAMOND, 10);
-    String ret = card.toString();
-    assertThat(ret, is("DT"));
-  }
-
-  @Test
-  public void toString_DIAMOND_11() {
-    card = new Card(Card.SUIT_DIAMOND, 11);
-    String ret = card.toString();
-    assertThat(ret, is("DJ"));
-  }
-
-  @Test
-  public void toString_DIAMOND_12() {
-    card = new Card(Card.SUIT_DIAMOND, 12);
-    String ret = card.toString();
-    assertThat(ret, is("DQ"));
-  }
-
-  @Test
-  public void toString_DIAMOND_13() {
-    card = new Card(Card.SUIT_DIAMOND, 13);
-    String ret = card.toString();
-    assertThat(ret, is("DK"));
-  }
-
-  @Test
-  public void toString_SPADE_1() {
-    card = new Card(Card.SUIT_SPADE, 1);
-    String ret = card.toString();
-    assertThat(ret, is("SA"));
-  }
-
-  @Test
-  public void toString_SPADE_2() {
-    card = new Card(Card.SUIT_SPADE, 2);
-    String ret = card.toString();
-    assertThat(ret, is("S2"));
-  }
-
-  @Test
-  public void toString_SPADE_3() {
-    card = new Card(Card.SUIT_SPADE, 3);
-    String ret = card.toString();
-    assertThat(ret, is("S3"));
-  }
-
-  @Test
-  public void toString_SPADE_4() {
-    card = new Card(Card.SUIT_SPADE, 4);
-    String ret = card.toString();
-    assertThat(ret, is("S4"));
-  }
-
-  @Test
-  public void toString_SPADE_5() {
-    card = new Card(Card.SUIT_SPADE, 5);
-    String ret = card.toString();
-    assertThat(ret, is("S5"));
-  }
-
-  @Test
-  public void toString_SPADE_6() {
-    card = new Card(Card.SUIT_SPADE, 6);
-    String ret = card.toString();
-    assertThat(ret, is("S6"));
-  }
-
-  @Test
-  public void toString_SPADE_7() {
-    card = new Card(Card.SUIT_SPADE, 7);
-    String ret = card.toString();
-    assertThat(ret, is("S7"));
-  }
-
-  @Test
-  public void toString_SPADE_8() {
-    card = new Card(Card.SUIT_SPADE, 8);
-    String ret = card.toString();
-    assertThat(ret, is("S8"));
-  }
-
-  @Test
-  public void toString_SPADE_9() {
-    card = new Card(Card.SUIT_SPADE, 9);
-    String ret = card.toString();
-    assertThat(ret, is("S9"));
-  }
-
-  @Test
-  public void toString_SPADE_10() {
-    card = new Card(Card.SUIT_SPADE, 10);
-    String ret = card.toString();
-    assertThat(ret, is("ST"));
-  }
-
-  @Test
-  public void toString_SPADE_11() {
-    card = new Card(Card.SUIT_SPADE, 11);
-    String ret = card.toString();
-    assertThat(ret, is("SJ"));
-  }
-
-  @Test
-  public void toString_SPADE_12() {
-    card = new Card(Card.SUIT_SPADE, 12);
-    String ret = card.toString();
-    assertThat(ret, is("SQ"));
-  }
-
-  @Test
-  public void toString_SPADE_13() {
-    card = new Card(Card.SUIT_SPADE, 13);
-    String ret = card.toString();
-    assertThat(ret, is("SK"));
-  }
-
-	@Test
-	public void コンストラクタ() throws Exception{
-    Card instance = new Card(Card.SUIT_CLUB, 1);
-    assertThat(instance.getSuit(), is(Card.SUIT_CLUB));
-    assertThat(instance.getNumber(), is(1));
-	}
 
 	// エントリーポイント
 	public static void main(String[] args){
